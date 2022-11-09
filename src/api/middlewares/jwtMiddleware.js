b350c8f7b07f9988d9b0904888438954fee1f0c9
Bois Julien <71647803+JulienBois@users.noreply.github.com>
2022-11-09 18:36:35 +0100

    initial commit

---
+++ src/api/middlewares/jwtMiddleware.js
@@ -0,0 +1,25 @@
+const jwt = require("jsonwebtoken");
+require('dotenv').config();
+
+const jwtKey = process.env.JWT_KEY;
+
+exports.verifyToken = (req, res, next) => {
+    let token = req.headers['authorization'];
+
+    if(token !== undefined) {
+        jwt.verify(token, jwtKey, (error, payload) => {
+            if(error) {
+                console.log(error)
+                res.status(403);
+                res.json({message : 'Accès interdit : token invalide'});
+            }
+            else {
+                next();                
+            }
+        })
+    }
+    else {
+        res.status(403);
+        res.json({message : "Accès interdit : token manquant"})
+    }
+}
